24bf24fa67a3cffb1ba4a513367eb29d3e139aca
BooneSchmucker <booneschmucker1987@gmail.com>
2021-10-14 14:58:23 -0600

    added balance_team function

--- application.py
+++ application.py
@@ -5,6 +5,10 @@ players_copy = copy.deepcopy(constants.PLAYERS)
 teams_copy = copy.deepcopy(constants.TEAMS)
 experienced_players = []
 inexperienced_players = []
+panthers = []
+bandits = []
+warriors = []
+
 
 def clean_data():
     for player in players_copy:
@@ -19,9 +23,21 @@ def clean_data():
     return experienced_players, inexperienced_players
 
 
+def balance_teams():
+    teams = [panthers, bandits, warriors]
+    num_teams = len(teams)
+    for num in range(len(experienced_players)):
+        teams[num % num_teams].append(experienced_players[num])
+    for num in range(len(inexperienced_players)):
+        teams[num % num_teams].append(inexperienced_players[num])
 
+        
+        
+    
+    
         
         
 
 if __name__ == "__main__":
     clean_data()
+    balance_teams()
