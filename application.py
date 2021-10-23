a3c6cc242813a749bd25140245934b9e1ec82194
BooneSchmucker <booneschmucker1987@gmail.com>
2021-10-23 17:07:06 -0600

    attempted height and guardian fix (failed)

--- application.py
+++ application.py
@@ -13,15 +13,15 @@ warriors = []
 
 def clean_data():
     for player in players_copy:
-        clean_height = int(player["height"].split(" ")[0])
-        clean_guardians = player["guardians"].split(" and ")
+        height = int(player["height"].split(" ")[0])
+        guardians = player["guardians"].split(" and ")
         if player["experience"] == "YES":
             player["experience"] = True
             experienced_players.append(player)
         elif player["experience"] == "NO":
             player["experience"] = False
             inexperienced_players.append(player)
-    return experienced_players, inexperienced_players, clean_height, clean_guardians
+    return player
 
 
 def balance_teams():
@@ -33,18 +33,18 @@ def balance_teams():
         teams[num % num_teams].append(inexperienced_players[num])
 
 
-
 def display_stats(team):
     total_players = len(team)
     players_names = []
-    guardians_names = [] #still displying "and" between guardians
+    guardians_names = [] # still displying "and" between guardians
     experienced_count = 0
     inexperienced_count = 0
-    for name in team:
-        players_names.append(name["name"])
-    for guardians in team:
-        guardians_names.append(guardians["guardians"])
+    team_heights = []    # not functioning
+    average_height = sum(team_heights) / len(team)
     for player in team:
+        players_names.append(player["name"])
+        guardians_names.append(player["guardians"])
+        team_heights.append(player["height"])
         if player["experience"] == True:
             experienced_count += 1
         else:
@@ -52,10 +52,12 @@ def display_stats(team):
     print("Total Players: {}".format(total_players))
     print("Experienced Players: {}".format(experienced_count))
     print("Rookies: {}".format(inexperienced_count))
+    print("Average Height: {}".format(average_height))
     print("\n**Players**")
     print(", ".join(players_names))
     print("\n**Guardians**")
     print(", ".join(guardians_names), "\n")
+    
 
 def console():
     print("Boones Basketball Stats Tool\n")
@@ -98,6 +100,7 @@ def console():
             elif prompt1 == 2:
                 break
     sys.exit("\nYou have exited the stats tool. See you next season!\n")
+
 
 if __name__ == "__main__":
     clean_data()
